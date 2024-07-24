044c9424c4f9b30054dfba16ad0be2b17cb27830
ExauceAk <exauceakplaka@gmail.com>
2024-07-24 08:09:27 +0100

    fix build

--- app/page.tsx
+++ app/page.tsx
@@ -4,22 +4,18 @@ import { redirect } from "next/navigation";
 
 export default function Home() {
   // redirect("tourist-guides/step1");
-  return <div className="flex h-screen bg-[url('/images/font.jpg')] bg-cover justify-center items-center  gap-4">
-    <Link
-      className=" text-secondary-500"
-      href="/tourist-guides/step1"
-    >
-      <Button className="bg-[#2ea2bd] hover:bg-[#2ea2bd]" >Guides Touristiques</Button>
-    </Link>
-    <Link
-      className=" text-secondary-500"
-      href="/partenaires-hotels/step1"
-    >
-      <Button className="bg-[#2ea2bd] hover:bg-[#2ea2bd]" >Partenaires d'Hôtels</Button>
-    </Link>
-
-
-  </div>;
+  return (
+    <div className="flex h-screen bg-[url('/images/font.jpg')] bg-cover justify-center items-center  gap-4">
+      <Link className=" text-secondary-500" href="/tourist-guides/step1">
+        <Button className="bg-[#2ea2bd] hover:bg-[#2ea2bd]">
+          Guides Touristiques
+        </Button>
+      </Link>
+      <Link className=" text-secondary-500" href="/partenaires-hotels/step1">
+        <Button className="bg-[#2ea2bd] hover:bg-[#2ea2bd]">
+          Partenaires d&apos;hôtels
+        </Button>
+      </Link>
+    </div>
+  );
 }
-
-
